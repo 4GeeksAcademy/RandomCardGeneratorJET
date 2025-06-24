fa2a8adc2f38de87423f4124bd5bbbff6d17df17
JoseEnri <jose.tocasuche@gmail.com>
2025-06-24 01:22:07 +0000

    almost done about 90%

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = generateRandomSuit()
+  const number = generateRandomNumber()
+  
+  document.querySelector(".top-suit").innerHTML=suits;
+    document.querySelector(".number").innerHTML=number;
+    document.querySelector(".bottom-suit").innerHTML=suits;
 };
+
+function generateRandomNumber () {
+  let number = ["A","2","3","4","5","6","7","8","9","10","K","Q","J"];
+  let indexNumbers = Math.floor(Math.random() * number.length);
+  console.log(indexNumbers)
+  return number[indexNumbers];
+};
+function generateRandomSuit () {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  return suits[indexSuit];
+}
